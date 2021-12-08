2054ec8817b0a98407432e320447476cdda2d20d
john_s <john.s@clarusway.com>
2021-12-08 22:49:29 +0300

    Lifecycle methods in class component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import TestComponent from './TestComponent';
 
 function App() {
+const [isVisible, setIsvisible] = useState(true)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CLARUSWAY</h1>
+      {isVisible ? 
+      <TestComponent surname= "green"/>
+      : null
+    }
+      <button onClick={() => setIsvisible(!isVisible) } >unmount</button>
     </div>
   );
 }
